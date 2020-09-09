ac7046e4fb9e837873a9373eb1f5936283e11d1f
jarvis <jarvisyeh@qq.com>
2020-09-09 14:39:46 +0800

    1

--- Policy.py
+++ Policy.py
@@ -203,8 +203,8 @@ def S1toS3():
         # sum up target flows size
         for i in range(len(response["flows"])):
             print flows[i]["match"]
-            print "eth_type" in flows[i]["match"]
-            print "eth_type" in flows[i]["match"] and flows[i]["match"]["eth_type"] == "0x0x800"
+            print "ip_proto" in flows[i]["match"]
+            print "ip_proto" in flows[i]["match"] and flows[i]["match"]["ip_proto"] == "0x06"
             if checkMatch(flows[i]["match"]):
                 print total_bit + str(total_byte)
                 total_bit += int(flows[i]["byteCount"]) * 8
